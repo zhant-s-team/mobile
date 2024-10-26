cdae389059aa95f937dfa02653a9076d7d9f25ff
ilandalmut <ilandalmut12@gmail.com>
2024-10-25 21:53:10 -0300

    Atualização

--- lib/views/app_homepage.dart
+++ lib/views/app_homepage.dart
@@ -35,7 +35,8 @@ class _AppHomePageState extends State<AppHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppStyles.backgroundColor, // Cor de fundo RGB(255, 255, 255, 1)
-      body: Stack(
+      body: 
+      Stack(
         children: [
           Positioned(
             top: 20.0,
@@ -110,6 +111,7 @@ class _AppHomePageState extends State<AppHomePage> {
               }),
             ),
           ),
+          
           Positioned(
             top: 275.0,
             left: 7.0,
@@ -360,6 +362,261 @@ class _AppHomePageState extends State<AppHomePage> {
                       ),
                     ),
                   ],
+                ),
+              ),
+            ),
+            
+            Positioned(
+            top: 525.0,
+            left: 7.0,
+            right: 7.0,
+            child: Container(
+              height: 240.0,
+              decoration: BoxDecoration(
+                color: Color(0xFFD9D9D9),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Stack(
+                children: [
+                  Positioned(
+                    top: 5.0,
+                    left: 10.0,
+                    child: Text(
+                      '#723   Levar madeira até em Tupi',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 30.0,
+                    left: 0,
+                    right: 0,
+                    child: Center(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.asset(
+                          'assets/3f96a2704d779772f4f4be2df72a4fbf.png',
+                          height: 94.0,
+                          width: 274.0,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 134.0,
+                    left: 10.0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Empresa: Local Fretes',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'Cidade: Piracicaba',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'Destino: Tupi Paulista',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                          ),
+                        ),
+                        SizedBox(height: 5.0),
+                        Text(
+                          'Porte do veículo: Médio',
+                          style: TextStyle(
+                            fontSize: 14,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 160.0,
+                    right: 20.0,
+                    child: SizedBox(
+                      width: 140.0,
+                      height: 40.0,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          setState(() {
+                            _isOverlayVisible = true;
+                          });
+                        },
+                        icon: _isAccepted
+                            ? Icon(
+                                Icons.check,
+                                color: Colors.green,
+                                size: 20.0,
+                              )
+                            : SizedBox.shrink(),
+                        label: Text(
+                          'Ver Mais',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromRGBO(44, 44, 44, 1),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          if (_isOverlayVisible)
+
+            Positioned.fill(
+              child: Container(
+                color: Color.fromRGBO(185, 185, 185, 0.894),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 20.0,
+                      right: 20.0,
+                      child: IconButton(
+                        icon: Icon(Icons.close),
+                        color: Colors.black,
+                        iconSize: 30.0,
+                        onPressed: () {
+                          setState(() {
+                            _isOverlayVisible = false;
+                          });
+                        },
+                      ),
+                    ),
+                    Positioned(
+                      top: 60.0,
+                      left: 30.0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Empresa: Local Fretes',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Cidade: Piracicaba',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Destino: Tupi Paulista',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Estado: Médio',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Porte do Veículo: Médio',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Tipo de Carga: Médio',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Distância de Trajeto: 354 Km',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Endereço: Rua do Porto, 123, Centro',
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 50.0, // 50px acima da parte de baixo da tela
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: SizedBox(
+                          width: 160.0,
+                          height: 40.0,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              setState(() {
+                                _isAccepted = true; // Define o estado de aceitação
+                                _saveAcceptanceStatus(_isAccepted); // Salva o estado
+                              });
+                            },
+                            icon: _isAccepted
+                                ? Icon(
+                                    Icons.check,
+                                    color: Colors.green,
+                                    size: 20.0,
+                                  )
+                                : SizedBox.shrink(), // Ícone condicional
+                            label: Text(
+                              'Aceitar Frete',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.0,
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color.fromRGBO(44, 44, 44, 1),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
                 ),
               ),
             ),
